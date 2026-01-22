5bd8a26ecf6410dc86222df4381cbd2f3df24557
salmon <ellieoomf@gmail.com>
2026-01-21 19:52:26 -0800

    redesign homepage + add changelog

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,1 @@
-let copying = false;
 
-function copyDiscord() {
-    if (!copying) {
-        copying = true;
-        const p = document.getElementById("discord");
-        navigator.clipboard.writeText("salmonswim");
-        const prevHTML = p.innerHTML;
-        p.innerHTML += " copied to clipboard";
-        setTimeout(function() {
-            p.innerHTML = prevHTML;
-            copying = false;
-        }, 1000);
-    }
-}
